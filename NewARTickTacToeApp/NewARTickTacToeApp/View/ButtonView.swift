ae28f9bd3cb02feed5f20b8917bcdfac29fdcd57
Ohara <nori.tukareta@gmail.com>
2020-10-12 16:12:19 +0900

    一時保存

--- NewARTickTacToeApp/NewARTickTacToeApp/View/ButtonView.swift
+++ NewARTickTacToeApp/NewARTickTacToeApp/View/ButtonView.swift
@@ -7,3 +7,45 @@
 //
 
 import Foundation
+import UIKit
+
+
+class MakeButton {
+    
+    func makeButton {
+        
+        //for文で全てのボタンの設定をする
+        for num in 1...9 {
+            var selectButtton: UIButton?
+            //switch文でどのボタンが押されたかの条件分岐
+            switch num {
+            case 1:
+                selectButtton = number1Button
+            case 2:
+                selectButtton = number2Button
+            case 3:
+                selectButtton = number3Button
+            case 4:
+                selectButtton = number4Button
+            case 5:
+                selectButtton = number5Button
+            case 6:
+                selectButtton = number6Button
+            case 7:
+                selectButtton = number7Button
+            case 8:
+                selectButtton = number8Button
+            case 9:
+                selectButtton = number9Button
+            default:
+                return
+            }
+            if startButtonOn == 0 {
+                //buttonの無効化
+                selectButtton?.isEnabled = false
+            } else if startButtonOn == 1 {
+                //buttonの有効化
+                selectButtton?.isEnabled = true
+            }
+    }
+}
