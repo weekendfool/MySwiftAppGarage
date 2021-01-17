5b7a189adcddb4af14f79a44f2653027d7733738
Ohara <nori.tukareta@gmail.com>
2021-01-17 13:23:10 +0900

    基本画面構成及びファイル作成

---
+++ AgentBlackAndWhiteApp/Pods/Target Support Files/Pods-AgentBlackAndWhiteApp-AgentBlackAndWhiteAppUITests/Pods-AgentBlackAndWhiteApp-AgentBlackAndWhiteAppUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_AgentBlackAndWhiteApp_AgentBlackAndWhiteAppUITests : NSObject
+@end
+@implementation PodsDummy_Pods_AgentBlackAndWhiteApp_AgentBlackAndWhiteAppUITests
+@end
---
+++ AgentBlackAndWhiteApp/Pods/Target Support Files/Pods-AgentBlackAndWhiteAppTests/Pods-AgentBlackAndWhiteAppTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_AgentBlackAndWhiteAppTests : NSObject
+@end
+@implementation PodsDummy_Pods_AgentBlackAndWhiteAppTests
+@end
