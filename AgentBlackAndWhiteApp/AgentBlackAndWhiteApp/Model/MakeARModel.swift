874017cc9ed68f8be8b41c0ab5078f8880cec0d4
Ohara <nori.tukareta@gmail.com>
2021-01-22 21:03:54 +0900

    ゲームUI作成

---
+++ AgentBlackAndWhiteApp/AgentBlackAndWhiteApp/Model/MakeARModel.swift
@@ -0,0 +1,9 @@
+//
+//  MakeAR.swift
+//  AgentBlackAndWhiteApp
+//
+//  Created by 尾原徳泰 on 2021/01/18.
+//  Copyright © 2021 尾原徳泰. All rights reserved.
+//
+
+import Foundation
---
+++ AgentBlackAndWhiteApp/AgentBlackAndWhiteApp/Model/PlayerModel.swift
@@ -0,0 +1,16 @@
+//
+//  Player.swift
+//  AgentBlackAndWhiteApp
+//
+//  Created by 尾原徳泰 on 2021/01/20.
+//  Copyright © 2021 尾原徳泰. All rights reserved.
+//
+
+import Foundation
+
+// ユーザーの変数宣言
+struct User {
+    var UserName = ""
+    var UserPassWord = ""
+    var UserImageNumber = ""
+}
