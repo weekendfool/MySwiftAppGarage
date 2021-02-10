082b4a1b6529e5b39f4b1a737b224d3086328122
Ohara <nori.tukareta@gmail.com>
2021-02-10 17:56:44 +0900

    userの辞書を見やすく修正

--- AgentBlackAndWhiteApp/AgentBlackAndWhiteApp/Model/PlayerModel.swift
+++ AgentBlackAndWhiteApp/AgentBlackAndWhiteApp/Model/PlayerModel.swift
@@ -17,6 +17,7 @@ struct User {
     var passWord = ""
     // ユーザーの登録アイコンに使用する画像名
     var iconImageNumber = ""
+    var userID = ""
     // ユーザーのルームID
     var roomID = ""
     // ユーザーがランダムマッチングで能動的検索者であるフラグ
@@ -27,12 +28,21 @@ struct User {
     var parmitRandomMatchRoomFlag = false
     
     // データベースに登録するユーザーの変数宣言
-    var userDatabaseDic: [String: Any] = ["userName": "", "passWord": "", "roomID": "", "waitRandomMatchFlag": false, "banWritingMatchingFlag": false, "parmitRandomMatchRoomFlag": false]
+    var userDatabaseDic: [String: Any] = [
+        "userName": "",
+        "passWord": "",
+        "userID": "",
+        "roomID": "",
+        "waitRandomMatchFlag": false,
+        "banWritingMatchingFlag": false,
+        "parmitRandomMatchRoomFlag": false
+    ]
     
     // データベースに登録するユーザーの情報を格納する
     mutating func registeruserDatabaseDic() {
         userDatabaseDic["userName"] = userName
         userDatabaseDic["passWord"] = passWord
+        userDatabaseDic["userID"] = userID
         userDatabaseDic["roomID"] = roomID
         userDatabaseDic["waitRandomMatchFlag"] = waitRandomMatchFlag
         userDatabaseDic["banWritingMatchingFlag"] = banWritingMatchingFlag
